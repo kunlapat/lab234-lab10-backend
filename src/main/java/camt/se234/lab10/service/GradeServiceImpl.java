dd3ded96270652b292786ac8479e58c41b588eb4
bizhop <kunlapat2@gmail.com>
2018-03-28 15:15:44 +0700

    Edit method

--- src/main/java/camt/se234/lab10/service/GradeServiceImpl.java
+++ src/main/java/camt/se234/lab10/service/GradeServiceImpl.java
@@ -7,7 +7,11 @@ import org.springframework.stereotype.Service;
 public class GradeServiceImpl implements GradeService {
     @Override
     public String getGrade(double score) {  
-            return "F";
-
+            if (score < 0 || score > 100) return "WRONG INPUT";
+            else if (score >= 80) return "A";
+            else if (score >= 70) return "B";
+            else if (score >= 60) return "C";
+            else if (score >= 50) return "D";
+            else return "F";
     }
 }
